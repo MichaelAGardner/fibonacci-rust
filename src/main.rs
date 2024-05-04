bd68f29ce0f2f72704c9b840a907c7667c42daea
MichaelAGardner <michael.gardner5@snhu.edu>
2024-05-04 17:13:15 -0500

    loop when getting all numbers

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,29 @@ use clap::Parser;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Cli{
-    #[arg(short='f', long="final", help="Print only the number requested")]
+struct Cli {
+    #[arg(short = 'f', long = "final", help = "Print only the number requested")]
     final_number_only: bool,
-    #[arg(short='a', long="all", help="Print all numbers up to number requested")]
+    #[arg(
+        short = 'a',
+        long = "all",
+        help = "Print all numbers up to number requested"
+    )]
     all_numbers: bool,
-    #[arg(required=true, value_name="fibonacci number")]
+    #[arg(required = true, value_name = "fibonacci number")]
     fibonacci_number: u32,
 }
 fn main() {
-    let cli =Cli::parse();
+    let cli = Cli::parse();
 
     if cli.final_number_only {
         println!("Printing final fibonacci number");
     }
 
     if cli.all_numbers {
-        println!("Printing all fibonacci numbers");
+        for i in 1..=cli.fibonacci_number {
+            println!("Printing all fibonacci numbers");
+        }
     }
 
     println!("number: {:?}", cli.fibonacci_number);
